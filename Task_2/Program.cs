cea7ca5a0f67dbcbe7940c90506e1fcab70aa95f
Alexandr <crazy.top@mail.ru>
2022-09-07 11:53:17 +0300

    Task complete

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Input coordinates: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+double y1 = Convert.ToInt32(Console.ReadLine());
+double z1 = Convert.ToInt32(Console.ReadLine());
+
+double x2 = Convert.ToInt32(Console.ReadLine());
+double y2 = Convert.ToInt32(Console.ReadLine());
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+double coordinates = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine(coordinates);
+
+
