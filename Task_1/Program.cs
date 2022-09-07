26f50a850b103b7fea8364d8271a19ad36286c8a
Alexandr <crazy.top@mail.ru>
2022-09-07 11:41:34 +0300

    Task complete

---
+++ Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Input a five-digit number: ");
+string array = Console.ReadLine();
+if(array.Length != 5)
+Console.WriteLine("error");
+else if(array[0] == array[4] && array[1] == array[3])
+Console.WriteLine("palindrome");
+else
+Console.WriteLine("no palindrome");
+
